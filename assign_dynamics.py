ff0fad5c4652d930a8b6b66ddd9ae2b0a30466a8
luhugerth <luisa.hugerth@scilifelab.se>
2023-04-06 11:35:47 +0200

    can now handle subtypes as well

--- assign_dynamics.py
+++ assign_dynamics.py
@@ -38,25 +38,22 @@ except:
 warnings.filterwarnings('ignore')
 
 
-
 #setting up expected arguments and help messages
 parser = argparse.ArgumentParser(description="SNAPPYNAME is a tool to classify vaginal time-series into dynamic categories")
 
 parser.add_argument("-i", "--input", help="Path to VALENCIA output",required=True)
 parser.add_argument("-m", "--metadata", help="CSV file with 'sampleID,subjectID,menses', where menses takes 1 for yes and 0 for no")
 parser.add_argument("-o","--output", help="Output csv file prefix", default="SNAPPYNAME.out.csv")
-parser.add_argument("-s", "--subtypes", help="Use CST subtypes instead of main types; requires eubiosis and/or dysbiosis argument", 
-                    action='store_true')
-parser.add_argument("-d", "--dysbiosis", help="comma-separated list of CST or sub-CST considered dysbiotic", default="III,IV-A,IV-B,IV-C,IV-D")
+parser.add_argument("-s", "--subtypes", help="Use CST subtypes instead of main types; requires eubiosis and dysbiosis argument", 
+                    action='store_true', default=False)
+parser.add_argument("-d", "--dysbiosis", help="comma-separated list of CST or sub-CST considered dysbiotic", default="III,IV-A,IV-B,IV-C")
 parser.add_argument("-e", "--eubiosis", help="comma-separated list of CST or sub-CST considered eubiotic", default="I,II,V")     
 args = parser.parse_args()
 
 
 ## Additional things to check:
 # If subtype, is eu/dys defined?
-# Are all samples in the metadata also in the VALENCIA data?
 
-## Need to implement all optional parameters
 # Step 1: read the Valencia output and store type for each sample
 try:
     valencia = pd.read_csv(args.input,sep=',') #change to args.input when wrapping
@@ -74,10 +71,13 @@ except:
     
 ## This function assigns for one individual at a time
 
-def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic):
+def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic, subtypes):
     allids = set(metadata.set_index(["subjectID"]).loc[(subjID)]["sampleID"])
     samples_per_subject = valencia[valencia["sampleID"].isin(allids)]
-    counts_subject = samples_per_subject.groupby("CST")[["sampleID"]].count()
+    if(subtypes):
+        counts_subject = samples_per_subject.groupby("subCST")[["sampleID"]].count()
+    else:
+        counts_subject = samples_per_subject.groupby("CST")[["sampleID"]].count()
   
     eubio = counts_subject[counts_subject.index.isin(eubiotic)].sum()
     dysbio = counts_subject[counts_subject.index.isin(dysbiotic)].sum()
@@ -91,8 +91,10 @@ def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic):
     else:
         midcycle = set(metadata.set_index(["subjectID", "menses"]).loc[(subjID,0)]["sampleID"])
         samples_per_subject = valencia[valencia["sampleID"].isin(midcycle)]
-        counts_subject = samples_per_subject.groupby("CST")[["sampleID"]].count()
-        counts_subject
+        if(subtypes):
+            counts_subject = samples_per_subject.groupby("subCST")[["sampleID"]].count()
+        else:    
+            counts_subject = samples_per_subject.groupby("CST")[["sampleID"]].count()
 
         eubio = counts_subject[counts_subject.index.isin(eubiotic)].sum()
         dysbio = counts_subject[counts_subject.index.isin(dysbiotic)].sum()
@@ -105,14 +107,26 @@ def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic):
             
 
 # test if all CST are accounted for; only implemented for main CST
-eu_cst = args.eubiosis.split(sep=",")
-dys_cst = args.dysbiosis.split(sep=",")
-all_cst = eu_cst + dys_cst
-#print(all_cst)
-if(set(all_cst) != {"I", "II", "III", "IV-A", "IV-B", "IV-C", "IV-D", "V"}):
-    sys.exit('The following CST must be included: "I", "II", "III", "IV-A", "IV-B", "IV-C", "IV-D", "V"')
-if(len(set(eu_cst).intersection(dys_cst))>0):
-    sys.exit("A CST cannot be eubiotic and dysbiotic at once")    
+if args.subtypes:
+    eu_cst = args.eubiosis.split(sep=",")
+    dys_cst = args.dysbiosis.split(sep=",")
+    all_cst = eu_cst + dys_cst
+    if(set(all_cst) != {"I-A", "I-B", "II-A", "II-B", "III-A", "III-B", "V",\
+            "IV-A", "IV-B", "IV-C0", "IV-C1", "IV-C2", "IV-C3", "IV-C4"}):
+        sys.exit('When using subtypes, the  following CST must be included: \
+                "I-A", "I-B", "II-A", "II-B", "III-A", "III-B",\
+                "IV-A", "IV-B", "IV-C0", "IV-C1", "IV-C2", "IV-C3", "IV-C4", "V"')
+    if(len(set(eu_cst).intersection(dys_cst))>0):
+        sys.exit("A CST cannot be eubiotic and dysbiotic at once")
+
+else:    
+    eu_cst = args.eubiosis.split(sep=",")
+    dys_cst = args.dysbiosis.split(sep=",")
+    all_cst = eu_cst + dys_cst
+    if(set(all_cst) != {"I", "II", "III", "IV-A", "IV-B", "IV-C", "V"}):
+        sys.exit('The following CST must be included: "I", "II", "III", "IV-A", "IV-B", "IV-C", "V"')
+    if(len(set(eu_cst).intersection(dys_cst))>0):
+        sys.exit("A CST cannot be eubiotic and dysbiotic at once")    
 
 # test if there are samples without metadata or without Valencia
 # if so, warn and proceed
@@ -131,7 +145,7 @@ allsubjects = metadata["subjectID"].unique()
 dynamics = list()
 for i in range(len(allsubjects)):
     subj = allsubjects[i]
-    dynamic = assign_dynamics(valencia, metadata, subj, eu_cst, dys_cst)
+    dynamic = assign_dynamics(valencia, metadata, subj, eu_cst, dys_cst, args.subtypes)
     dynamics.append(dynamic)
         
 to_print = pd.DataFrame(dynamics, allsubjects, columns = ["Dynamics"])
