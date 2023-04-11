e8d02103e759eea3c8111e1ee02ef5ec3ec8ebbb
Fredrik Boulund <fredrik.boulund@ki.se>
2023-04-11 15:55:02 +0200

    Refactor main functionality

--- assign_dynamics.py
+++ assign_dynamics.py
@@ -74,13 +74,14 @@ def parse_args():
     
     args = parser.parse_args()
 
-## This function assigns for one individual at a time
-
     return args
 
+
 def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic, subtypes):
-    allids = set(metadata.set_index(["subjectID"]).loc[(subjID)]["sampleID"])
-    samples_per_subject = valencia[valencia["sampleID"].isin(allids)]
+    """Assign dynamics classification for one individual
+    """
+    all_ids = set(metadata.set_index(["subjectID"]).loc[(subjID)]["sampleID"])
+    samples_per_subject = valencia[valencia["sampleID"].isin(all_ids)]
     if subtypes:
         counts_subject = samples_per_subject.groupby("subCST")[["sampleID"]].count()
     else:
@@ -88,18 +89,19 @@ def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic, subtypes):
 
     eubio = counts_subject[counts_subject.index.isin(eubiotic)].sum()
     dysbio = counts_subject[counts_subject.index.isin(dysbiotic)].sum()
-    eu_rat = float(eubio / (eubio + dysbio))
-    dys_rat = float(dysbio / (eubio + dysbio))
+    eu_rate = float(eubio / (eubio + dysbio))
+    dys_rate = float(dysbio / (eubio + dysbio))
 
-    if eu_rat >= 0.8:
+    if eu_rate >= 0.8:
         return "Constant eubiotic"
-    elif dys_rat >= 0.8:
+    elif dys_rate >= 0.8:
         return "Constant dysbiotic"
     else:
         midcycle = set(
             metadata.set_index(["subjectID", "menses"]).loc[(subjID, 0)]["sampleID"]
         )
         samples_per_subject = valencia[valencia["sampleID"].isin(midcycle)]
+
         if subtypes:
             counts_subject = samples_per_subject.groupby("subCST")[["sampleID"]].count()
         else:
@@ -107,88 +109,95 @@ def assign_dynamics(valencia, metadata, subjID, eubiotic, dysbiotic, subtypes):
 
         eubio = counts_subject[counts_subject.index.isin(eubiotic)].sum()
         dysbio = counts_subject[counts_subject.index.isin(dysbiotic)].sum()
-        eu_rat = float(eubio / (eubio + dysbio))
+        eu_rate = float(eubio / (eubio + dysbio))
 
-        if eu_rat >= 0.8:
+        if eu_rate >= 0.8:
             return "Menses dysbiotic"
         else:
             return "Unstable"
 
 
-# test if all CST are accounted for; only implemented for main CST
-if args.subtypes:
-    eu_cst = args.eubiosis.split(sep=",")
-    dys_cst = args.dysbiosis.split(sep=",")
-    all_cst = eu_cst + dys_cst
-    if set(all_cst) != {
-        "I-A",
-        "I-B",
-        "II-A",
-        "II-B",
-        "III-A",
-        "III-B",
-        "V",
-        "IV-A",
-        "IV-B",
-        "IV-C0",
-        "IV-C1",
-        "IV-C2",
-        "IV-C3",
-        "IV-C4",
-    }:
-        sys.exit(
-            'When using subtypes, the  following CST must be included: \
-                "I-A", "I-B", "II-A", "II-B", "III-A", "III-B",\
-                "IV-A", "IV-B", "IV-C0", "IV-C1", "IV-C2", "IV-C3", "IV-C4", "V"'
-        )
-    if len(set(eu_cst).intersection(dys_cst)) > 0:
-        sys.exit("A CST cannot be eubiotic and dysbiotic at once")
+def validate_csts(eubiosis, dysbiosis, subtypes=False):
+    """Validate that all CST are present in either eubiosis and dysbiosis classes.
+    """
+    eu_cst = set(eubiosis.split(sep=","))
+    dys_cst = set(dysbiosis.split(sep=","))
+    all_cst = eu_cst.union(dys_cst)
 
-else:
-    eu_cst = args.eubiosis.split(sep=",")
-    dys_cst = args.dysbiosis.split(sep=",")
-    all_cst = eu_cst + dys_cst
-    if set(all_cst) != {"I", "II", "III", "IV-A", "IV-B", "IV-C", "V"}:
-        sys.exit(
-            'The following CST must be included: "I", "II", "III", "IV-A", "IV-B", "IV-C", "V"'
-        )
-    if len(set(eu_cst).intersection(dys_cst)) > 0:
-        sys.exit("A CST cannot be eubiotic and dysbiotic at once")
+    cst_both_eu_and_dys = eu_cst.intersection(dys_cst)
+    if cst_both_eu_and_dys:
+        print(f"A CST cannot be eubiotic and dysbiotic at once: {cst_both_eu_and_dys}")
+        sys.exit(1)
 
-# test if there are samples without metadata or without Valencia
-# if so, warn and proceed
-all_meta_ids = set(metadata["sampleID"])
-all_val_ids = set(valencia["sampleID"])
-only_meta = all_meta_ids.difference(all_val_ids)
-only_val = all_val_ids.difference(all_meta_ids)
-if len(only_meta) > 0:
-    print(
-        " ".join(
-            [
-                "Warning:",
-                str(len(only_meta)),
-                "sampleIDs in metadata not found in the Valencia table",
-            ]
-        )
+    if subtypes:
+        if all_cst != ALL_SUBTYPE_CSTs:
+            print(f"ERROR: When using subtypes, the following CSTs must be included: {ALL_SUBTYPE_CSTs}")
+            sys.exit(1)
+    else:
+        if all_cst != ALL_CSTs:
+            print(f"ERROR: The following CST must be included: {ALL_CSTs}")
+            sys.exit(1)
+
+    return eu_cst, dys_cst
+
+
+def check_sampleid_overlaps(metadata, valencia):
+    """Check that sample IDs exist in both metadata and VALENCIA data.
+
+    If there are samples without either metadata or VALENCIA data, print
+    warnings and proceed.
+    """
+    all_meta_ids = set(metadata["sampleID"])
+    all_val_ids = set(valencia["sampleID"])
+    only_meta = all_meta_ids.difference(all_val_ids)
+    only_val = all_val_ids.difference(all_meta_ids)
+    if len(only_meta) > 0:
+        print(f"WARNING: {len(only_meta)} sampleIDs in metadata not found in the Valencia table!")
+    if len(only_val) > 0:
+        print(f"WARNING: {len(only_val)} sampleIDs in VALENCIA output not found in the metadata!")
+
+
+def main(input, metadata, eubiosis, dysbiosis, subtypes):
+    # Step 1: read the Valencia output and store type for each sample
+    try:
+        valencia = pd.read_csv(input, sep=",")
+    except Exception as e:
+        print(e)
+        print(f"ERROR: Please provide a valid path to the VALENCIA output using -i")
+        exit(1)
+
+    # Step 2: read the metadata file
+    try:
+        metadata = pd.read_csv(metadata, sep=",")
+    except Exception as e:
+        print(e)
+        print(f"ERROR: Please provide a valid path to the metadata using -m")
+        exit(1)
+
+    eu_cst, dys_cst = validate_csts(eubiosis, dysbiosis, subtypes)
+    check_sampleid_overlaps(metadata, valencia)
+
+    allsubjects = metadata["subjectID"].unique()
+    dynamics = list()
+    for i in range(len(allsubjects)):
+        subj = allsubjects[i]
+        dynamic = assign_dynamics(valencia, metadata, subj, eu_cst, dys_cst, subtypes)
+        dynamics.append(dynamic)
+
+    valody_classifications = pd.DataFrame(dynamics, allsubjects, columns=["Dynamics"])
+    valody_classifications.to_csv(args.output, index_label="SampleID", sep=",")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    if Path(args.output).exists() and Path(args.output).is_file():
+        print(f"WARNING: Overwriting output file: {args.output}")
+
+    main(
+        args.input,
+        args.metadata,
+        args.eubiosis,
+        args.dysbiosis,
+        args.subtypes,
     )
-if len(only_val) > 0:
-    print(
-        " ".join(
-            [
-                "Warning:",
-                str(len(only_val)),
-                "sampleIDs in Valencia output not found in the metadata",
-            ]
-        )
-    )
-
-
-allsubjects = metadata["subjectID"].unique()
-dynamics = list()
-for i in range(len(allsubjects)):
-    subj = allsubjects[i]
-    dynamic = assign_dynamics(valencia, metadata, subj, eu_cst, dys_cst, args.subtypes)
-    dynamics.append(dynamic)
-
-to_print = pd.DataFrame(dynamics, allsubjects, columns=["Dynamics"])
-to_print.to_csv(args.output, sep=",")
